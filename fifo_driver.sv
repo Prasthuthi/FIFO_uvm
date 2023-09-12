6083d354b97994a36000dbb95f13221b3ee33d15
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2023-09-12 12:34:28 +0530

    Update fifo_driver.sv

--- fifo_driver.sv
+++ fifo_driver.sv
@@ -14,14 +14,16 @@ class fifo_driver extends uvm_driver#(fifo_seq_item);
   endfunction
 
   virtual task run_phase(uvm_phase phase);
+    begin
     vif.driver_mp.driver_cb.i_wren <= 'b0;
     vif.driver_mp.driver_cb.i_rden <= 'b0;
     vif.driver_mp.driver_cb.i_wrdata <= 'b0;
+    end
     forever begin
       seq_item_port.get_next_item(req);
       if(req.i_wren == 1)
         main_write(req.i_wrdata);
-      else if(req.i_rden == 1)
+      if(req.i_rden == 1)
         main_read();
       seq_item_port.item_done();
     end
