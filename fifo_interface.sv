20692f35decb2122c85c55c2d8fcb5b47fc32129
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2023-09-12 12:16:36 +0530

    Update fifo_interface.sv

--- fifo_interface.sv
+++ fifo_interface.sv
@@ -6,10 +6,9 @@ interface fifo_interface(input clk,rstn);
     bit o_full,o_empty,o_alm_full,o_alm_empty,i_wren,i_rden;
     bit [127:0] i_wrdata;
     bit [127:0] o_rddata;
-endinterface : dut_if
 
 clocking driver_cb @(posedge clk);
-    default input #1 output #1;
+    default input #0 output #0;
     output i_wren;
     output i_rden;
     output i_wrdata;
@@ -41,3 +40,4 @@ modport driver_mp (input clk, rstn, clocking driver_cb);
       modport passive_mon_mp (input clk, rstn, clocking passive_mon_cb);
     
 endinterface
+
