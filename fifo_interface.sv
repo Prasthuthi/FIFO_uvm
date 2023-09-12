bbc0feb39c4e067cc78a1d8af5e4ac6217a30502
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2023-09-12 12:06:24 +0530

    Update fifo_interface.sv

--- fifo_interface.sv
+++ fifo_interface.sv
@@ -2,23 +2,32 @@ import uvm_pkg::*;
 `include "uvm_macros.svh"
 `timescale 1ns/10ps
 
-/***********************************************
-* interface 
-*********************************************/
-interface dut_if(clk,rstn);
-    logic o_full,o_empty,o_alm_full,o_alm_empty,i_wren,i_rden;
-    logic [127:0] i_wrdata;
-    logic [127:0] o_rddata;
+interface fifo_interface(input clk,rstn);
+    bit o_full,o_empty,o_alm_full,o_alm_empty,i_wren,i_rden;
+    bit [127:0] i_wrdata;
+    bit [127:0] o_rddata;
 endinterface : dut_if
 
-clocking d_cb @(posedge clk);
+clocking driver_cb @(posedge clk);
     default input #1 output #1;
     output i_wren;
     output i_rden;
     output i_wrdata;
+    input o_full;
+    input o_empty;
+    input o_alm_full;
+    input o_alm_empty;
+    input o_rddata;
   endclocking
 
- clocking m_cb @(posedge clk);
+clocking active_mon_cb @(posedge clk);
+   default input #0 output #0;
+    input i_wren;
+    input i_rden;
+    input i_wrdata;
+  endclocking
+
+ clocking passive_mon_cb @(posedge clk);
     default input #1 output #1;
     input o_full;
     input o_empty;
@@ -27,7 +36,8 @@ clocking d_cb @(posedge clk);
    input o_rddata;
   endclocking
 
-modport d_mp (input clk, rstn, clocking d_cb);
-  modport m_mp (input clk, rstn, clocking m_cb);
+modport driver_mp (input clk, rstn, clocking driver_cb);
+    modport active_mon_mp (input clk, rstn, clocking active_mon_cb);
+      modport passive_mon_mp (input clk, rstn, clocking passive_mon_cb);
     
 endinterface
