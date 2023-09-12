474282cf43f01ceca44601ed577221e3c3b480b2
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2023-09-12 15:29:58 +0530

    Update fifo_agent.sv

--- fifo_agent.sv
+++ fifo_agent.sv
@@ -4,23 +4,23 @@
 `include "fifo_driver.sv"
 `include "fifo_monitor.sv"
 
-class f_agent extends uvm_agent;
-  f_sequencer f_seqr;
-  f_driver f_dri;
-  f_monitor f_mon;
-  `uvm_component_utils(f_agent)
+class fifo_agent extends uvm_agent;
+  fifo_sequencer f_seqr;
+  fifo_driver f_dri;
+  fifo_monitor f_mon;
+  `uvm_component_utils(fifo_agent)
   
-  function new(string name = "f_agent", uvm_component parent);
+  function new(string name = "fifo_agent", uvm_component parent);
     super.new(name, parent);
   endfunction
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     if(get_is_active() == UVM_ACTIVE) begin
-      f_seqr = f_sequencer::type_id::create("f_seqr", this);
-      f_dri = f_driver::type_id::create("f_dri", this);
+      f_seqr = fifo_sequencer::type_id::create("f_seqr", this);
+      f_dri = fifo_driver::type_id::create("f_dri", this);
     end
-    f_mon = f_monitor::type_id::create("f_mon", this);
+    f_mon = fifo_monitor::type_id::create("f_mon", this);
   endfunction
   
   virtual function void connect_phase(uvm_phase phase);
